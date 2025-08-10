29b9dde0d88214364e24828a6b3a4781c0fa6f79
Tiago de Andrade Freire <tiago.freire@cubos.io>
2025-08-10 01:55:43 -0300

    fix: using ssh url as origin of app repository

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ async function main() {
 
     logStepSuccess(`Repository successfully created at ${repositoryUrlOutput}`)
 
-    await execCommand(`git remote add origin ${repositoryUrl}`, {
+    await execCommand(`git remote add origin ${repo.ssh_url}`, {
       cwd: projectPath,
     })
   }
